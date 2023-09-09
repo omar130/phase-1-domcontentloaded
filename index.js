3cfe5cace3dbe0d1ccaedeca4f75dba5c40024f3
Omar Richards <omarrichards01@gmail.com>
2023-09-09 19:25:45 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The Dom has Loaded");
+  document.getElementById("text").textContent = "This is really cool!"
+});
+
